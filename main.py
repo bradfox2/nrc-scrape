55e2626e5c40cd0b1b6e071ba9b6f2a5737cbd71
bradfox2 <bradfox2@penguin>
2020-02-17 23:05:21 -0700

    enr working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 
 import calendar
 import re
@@ -10,7 +11,8 @@ from bs4 import BeautifulSoup, NavigableString, Tag
 from requests.exceptions import HTTPError
 
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
 
 def get_text_after_tag(segment, tag: str) -> List:
@@ -41,113 +43,119 @@ def chunks(l, n):
 def get_main_table(page_html):
     return page_html.find(id='mainSubFull').find_all('table')
 
+
 def split_main_sub_2_tables(page_html):
     return page_html.find(id='mainSubFull').find_all('table')[
     0].find_all('table')
 
+
 def get_event_report_numbers(er_tables_html):
-    er_num_table_rows = er_tables_html.find_all(href = re.compile('#en'))
+    er_num_table_rows = er_tables_html.find_all(href=re.compile('#en'))
     return [int(x.text) for x in er_num_table_rows]
+
 
 def event_categorical_info_html_from_demarc_tag(tag):
     # correct table appears to be 4 sibling away, and the first table that shows up
     return tag.find_next_siblings('table')[0]
 
+
 def event_status_info_html_from_demarc_tag(tag):
     # correct table appears to be 4 sibling away, and the second table that shows up
     return tag.find_next_siblings('table')[1]
+
 
 def event_desc_html_from_demarc_tag(tag):
     # correct table appears to be 4 sibling away, and the third table that shows up
     return tag.find_next_siblings('table')[2]
 
-from abc import ABC, abstractmethod
+
 class EventInfo(ABC):
     def __init__(self, table_html):
         self.table_html = table_html
 
     def __repr__(self):
         return self.table_html.prettify()
-    
+
     @abstractmethod
     def parse_table_html(self):
         pass
 
+
 class EventCategoricalInfo(EventInfo):
     def __init__(self, table_html):
         super().__init__(table_html)
-        self.table_cols = self.table_html.find_all('td', {"align":"left"})
+        self.table_cols = self.table_html.find_all('td', {"align": "left"})
         self.info = {}
         self.emer_info = []
         self.person_info = []
         self.event_number = None
         self.parse_table_html()
-    
+
     def parse_table_html(self):
-        #3x2 table, usually
-        #1,1
+        # 3x2 table, usually
+        # 1,1
         self.info['er_type'] = self._parse_er_type()
-        #2,1 - rows delinated by <br> tags
+        # 2,1 - rows delinated by <br> tags
         ei = []
-        for segment in self.table_cols[1:4]: 
+        for segment in self.table_cols[1:4]:
             ei.extend(get_text_without_tag(segment, 'br'))
         for text in ei:
-            t = text.split(':',1)
+            t = text.split(':', 1)
             if t[0] in {'Region', 'City'}:
-                
-                # NRC has put some colon seperated fields in line or 
-                # seperated by line break into a table row, need to 
+
+                # NRC has put some colon seperated fields in line or
+                # seperated by line break into a table row, need to
                 # parse the row and seperate the 2nd colon seperated
                 # field from the first
 
                 # check for the state field in the Region or City rows
                 self._check_and_get_geo_state_field(t)
-                
+
                 # If a 2nd field is found (e.g. state) add it back to
-                # string data list for processing and then remove from 
+                # string data list for processing and then remove from
                 # current colon sep value dict value, 2nd col sep field is
-                # still a 'something: value' string, and so it is the last 
+                # still a 'something: value' string, and so it is the last
                 # item on list
                 ei.append(t[-1])
                 t.pop()
 
-            #store remaining fields 
+            # store remaining fields
             if len(t) > 1:
-                k,v = t
+                k, v = t
                 self.info[k] = v.strip()
 
         self.emer_info = self._parse_emergency_info(self.table_cols[4])
         self.person_info = self._parse_person_info(self.table_cols[5])
-        self.event_number = int(self.info.get('Event Number', None).strip())   
-        
+        self.event_number = int(self.info.get('Event Number', None).strip())
+
     def _parse_er_type(self):
         return self.table_cols[0].text
-    
+
     def _parse_cat_contact_info():
         pass
-    
+
     def _parse_cat_time_info():
         pass
-    
+
     def _parse_person_info(self, segment):
         return self._parse_stacked_line_breaks(segment)
 
-    def _parse_emergency_info(sel,segment):
+    def _parse_emergency_info(sel, segment):
         emergency_class_text = [segment.find('br').previous_sibling.strip()]
-        cfrs = ['10_cfr_sections',[]]
+        cfrs = ['10_cfr_sections', []]
         for br in segment.find_all('br')[1:]:
             next_text = br.next_sibling
-            if not (next_text and isinstance(next_text,NavigableString)):
+            if not (next_text and isinstance(next_text, NavigableString)):
                 continue
             next_text2 = next_text.next_sibling
             if next_text2 and isinstance(next_text2, Tag) and next_text2.name == 'br':
                 text = str(next_text).strip()
                 if text:
-                    cfrs[1].append(next_text.strip()) 
+                    cfrs[1].append(next_text.strip())
         return emergency_class_text + cfrs
-    
+
     def _parse_stacked_line_breaks(self, segment):
-        cfrs = [segment.find('br').previous_sibling.strip(),[]]
+        cfrs = [segment.find('br').previous_sibling.strip(), []]
         for br in segment.find_all('br'):
             next_text = br.next_sibling
             if not (next_text and isinstance(next_text, NavigableString)):
@@ -156,17 +164,18 @@ class EventCategoricalInfo(EventInfo):
             if next_text2 and isinstance(next_text2, Tag) and next_text2.name == 'br':
                 text = str(next_text).strip()
                 if text:
-                    cfrs[1].append(next_text.strip()) 
+                    cfrs[1].append(next_text.strip())
         return cfrs
-    
-    def _check_and_get_geo_state_field(self, texts:List[str]):
+
+    def _check_and_get_geo_state_field(self, texts: List[str]):
         for idx, text in enumerate(texts):
-            state_exist:int = text.find('\xa0State')
+            state_exist: int = text.find('\xa0State')
             if state_exist != -1:
                 texts.remove(text)
                 prev_val, state_kv = text.split('\xa0')
                 texts.append(prev_val)
-                texts.append(state_kv)     
+                texts.append(state_kv)
+
 
 class EventStatusInfo(EventInfo):
     def __init__(self, table_html):
@@ -180,29 +189,35 @@ class EventStatusInfo(EventInfo):
     def parse_table_html(self):
 
         header = self.table_rows[0]
-        
+
         self.col_names = [col.text for col in header.find_all('td')]
 
         if len(self.col_names) == 0: return None
-        
+
         # rows to list of dicts
-        self.unit_table = [{self.col_names[idx]:col.text for idx, col in enumerate(row.find_all('td'))} for row in self.table_rows[1:]]
+        self.unit_table = [{self.col_names[idx]:col.text for idx, col in enumerate(
+            row.find_all('td'))} for row in self.table_rows[1:]]
 
         return self.unit_table
+
 
 class EventDescInfo(EventInfo):
     def __init__(self, table_html):
         super().__init__(table_html)
         self.info = self.parse_table_html()
-    
+
     def parse_table_html(self):
         return get_text_without_tag(self.table_html, 'br')
 
+
 class Event(object):
     def __init__(self, demarcation_tag):
-        self._event_info_html = event_categorical_info_html_from_demarc_tag(demarcation_tag)
-        self._event_status_html = event_status_info_html_from_demarc_tag(demarcation_tag)
-        self._event_desc_html = event_desc_html_from_demarc_tag(demarcation_tag)
+        self._event_info_html = event_categorical_info_html_from_demarc_tag(
+            demarcation_tag)
+        self._event_status_html = event_status_info_html_from_demarc_tag(
+            demarcation_tag)
+        self._event_desc_html = event_desc_html_from_demarc_tag(
+            demarcation_tag)
 
         self.eci = EventCategoricalInfo(self._event_info_html)
         self.esi = EventStatusInfo(self._event_status_html)
@@ -211,59 +226,66 @@ class Event(object):
     def __repr__(self):
         return f'Event Num: {self.eci.event_number}'
 
+
 class EventNotificationReport(object):
     def __init__(self, page_html):
         self.date = page_html.find(id='mainSubFull').find_all('table')[
             0].find('h1').text
-        self.events = []
-        self._page_html = page_html
+        self.main_table = self.get_main_table(page_html)
+        self.events=self.get_events_from_main(self.main_table)
+        self._page_html=page_html
+
+    def __repr__(self):
+        return f'ENR from {str(self.date)}'
 
     def get_main_table(self, page_html):
-        return page_html.find(id='mainSubFull').find_all('table')
+        return page_html.find(id = 'mainSubFull').find_all('table')
 
     @classmethod
     def get_events_from_main(cls, main_table):
         # tables appear to have a name that follows <a name="en{eventnumber}"></a>
-        er_demarcation_tags = main_table[0].find_all('a', {"name":re.compile('en')})
-        event_tables_html = []
+        er_demarcation_tags=main_table[0].find_all(
+            'a', {"name": re.compile('en')})
+        event_tables_html=[]
 
         for tag in er_demarcation_tags:
-            e = Event(tag)
+            e=Event(tag)
             event_tables_html.append(e)
         return event_tables_html
-    
+
     @classmethod
     def from_url(cls, url: str, headers) -> EventNotificationReport:
-        req = None
-        attempts = 0
+        req=None
+        attempts=0
         while req is None and attempts < 5:
-            req = requests.get(url, timeout=5, headers=headers)
+            req=requests.get(url, timeout = 5, headers = headers)
         req.raise_for_status()
         if not req:
             raise ValueError('Unable to fetch url')
-        page_html = BeautifulSoup(req.content, 'html.parser')
+        page_html=BeautifulSoup(req.content, 'html.parser')
         return cls(page_html)
+
 
 # get all dates
 def build_nrc_event_report_url(year, month, day):
-    month, day = str(month).zfill(2), str(day).zfill(2)
-    url = f'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/{year}/{year}{month}{day}en.html'
+    month, day=str(month).zfill(2), str(day).zfill(2)
+    url=f'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/{year}/{year}{month}{day}en.html'
     return url
 
 def generate_nrc_event_report_urls():
-    dates = {}
+    dates={}
     for year in range(2003, 2020):
         # years before 2003 are in a weird format
         for month in range(1, 13):
-            day_range = calendar.monthrange(year, month)
+            day_range=calendar.monthrange(year, month)
             for day in range(day_range[0], day_range[1]):
-                dates[(year, month, day)] = build_nrc_event_report_url(
+                dates[(year, month, day)]=build_nrc_event_report_url(
                     year, month, day)
     return dates
 
 if __name__ == "__main__":
-    
-    url = 'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/2005/20050606en.html'
+
+    url='https://www.nrc.gov/reading-rm/doc-collections/event-status/event/2005/20050606en.html'
 
     assert build_nrc_event_report_url(
         2004, 12, 30) == 'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/2004/20041230en.html'
@@ -271,11 +293,13 @@ if __name__ == "__main__":
     assert build_nrc_event_report_url(
         2004, 2, 3) == 'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/2004/20040203en.html'
 
-    req = requests.get(url, headers=headers)
-    page_html = BeautifulSoup(req.content, 'html.parser')
-    main_table = get_main_table(page_html)
-    events = get_events_from_main(main_table)
-    events
+    e = EventNotificationReport.from_url(url, headers)
+
+    # req = requests.get(url, headers=headers)
+    # page_html = BeautifulSoup(req.content, 'html.parser')
+    # main_table = get_main_table(page_html)
+    # events = get_events_from_main(main_table)
+    # events
 
     # er_urls = generate_nrc_event_report_urls()
 
@@ -284,7 +308,7 @@ if __name__ == "__main__":
 #    urls = list(er_urls.values())[800:810]
 
  #   headers = {
-        #'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+        # 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
     # loop the urls and skip any 404s
  #   for url in urls:
@@ -313,6 +337,3 @@ if __name__ == "__main__":
     #     event_text:List[str] = get_text_without_tag(event_description_table,'br')
     #     event_text:List[str] = list(filter(lambda x: x if x!='' else None, event_text))
     #     event_title_text:str = event_text[0]
-
-
-
