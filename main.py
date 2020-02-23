58ce7e6b19150f4a4dd32e4f2d575e838003acfb
brad fox <bradfox2@gmail.com>
2020-02-23 11:32:03 -0700

    format

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import requests
 from bs4 import BeautifulSoup, NavigableString, Tag
 from requests.exceptions import HTTPError
 
-#use webbrowser headers to stop nrc security stuff from killing our requests
+# use webbrowser headers to stop nrc security stuff from killing our requests
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
@@ -51,7 +51,7 @@ def get_main_table(page_html):
 
 def split_main_sub_2_tables(page_html):
     return page_html.find(id='mainSubFull').find_all('table')[
-    0].find_all('table')
+        0].find_all('table')
 
 
 def get_event_report_numbers(er_tables_html):
@@ -64,6 +64,7 @@ def event_html_from_demarc_tag(tag, table_num):
     Primarily used to get the html table under the event demarcation tag. 
     '''
     return tag.find_next_siblings('table')[table_num]
+
 
 class EventInfo(ABC):
     def __init__(self, table_html):
@@ -86,7 +87,7 @@ class EventCategoricalInfo(EventInfo):
         self.person_info = []
         self.event_number = None
         self.parse_table_html()
-    
+
     def __repr__(self):
         pp = pprint.PrettyPrinter()
         return pp.ppformat(self.info)
@@ -127,14 +128,18 @@ class EventCategoricalInfo(EventInfo):
         self.emer_info = self._parse_emergency_info(self.table_cols[4])
         self.person_info = self._parse_person_info(self.table_cols[5])
         self.event_number = int(self.info.get('Event Number', None))
-        self.event_date = dp.parse(self.info.get('Event Date', None), fuzzy=True).date()
+        self.event_date = dp.parse(self.info.get(
+            'Event Date', None), fuzzy=True).date()
         self.event_time = self._parse_time(self.info.get('Event Time', None))
         self.facility = self._get_from_self_info('Facility')
         self.hq_ops_officer = self._get_from_self_info('HQ OPS Officer')
-        self.last_update_date = dp.parse(self._get_from_self_info('Last Update Date'), fuzzy=True).date()
+        self.last_update_date = dp.parse(
+            self._get_from_self_info('Last Update Date'), fuzzy=True).date()
         self.nrc_notified_by = self._get_from_self_info('NRC Notified By')
-        self.notification_date = dp.parse(self._get_from_self_info('Notification Date'), fuzzy=True).date()
-        self.notification_time = self._parse_time(self._get_from_self_info('Notification Time'))
+        self.notification_date = dp.parse(
+            self._get_from_self_info('Notification Date'), fuzzy=True).date()
+        self.notification_time = self._parse_time(
+            self._get_from_self_info('Notification Time'))
         self.rx_type = self._get_from_self_info('RX Type')
         self.region = self._get_from_self_info('Region')
         self.state = self._get_from_self_info('State')
@@ -144,7 +149,6 @@ class EventCategoricalInfo(EventInfo):
         self.city = self._get_from_self_info('City')
         self.county = self._get_from_self_info('County')
         self.rep_org = self._get_from_self_info('Rep Org')
-        
 
     def _parse_date(self, date_string):
         try:
@@ -152,21 +156,22 @@ class EventCategoricalInfo(EventInfo):
         except ValueError:
             Warning(f'Unable to parse {date_string} into datetime object.')
             return date_string
-        else: 
+        else:
             raise
-    
+
     def _parse_time(self, time_string):
 
-        tzinfos = {"EST": tz.gettz('EST'),"EDT": tz.gettz('EST'), "ET":tz.gettz('EST'), 'PDT':tz.gettz('America/Los_Angeles'), 'CST': tz.gettz('America/Chicago'), 'CDT': tz.gettz('America/Chicago'), 'MDT':tz.gettz('America/Denver'), "PST": tz.gettz('America/Los_Angeles'), "MST": tz.gettz('America/Denver')}
+        tzinfos = {"EST": tz.gettz('EST'), "EDT": tz.gettz('EST'), "ET": tz.gettz('EST'), 'PDT': tz.gettz('America/Los_Angeles'), 'CST': tz.gettz('America/Chicago'),
+                   'CDT': tz.gettz('America/Chicago'), 'MDT': tz.gettz('America/Denver'), "PST": tz.gettz('America/Los_Angeles'), "MST": tz.gettz('America/Denver')}
 
         try:
-            dte = dp.parse(time_string, fuzzy=True, tzinfos = tzinfos).time()
+            dte = dp.parse(time_string, fuzzy=True, tzinfos=tzinfos).time()
         except ValueError:
             Warning(f'Unable to parse {time_string} into datetime object.')
             return time_string
 
     def _get_from_self_info(self, field):
-        return self.info.get(field,None)
+        return self.info.get(field, None)
 
     def _parse_er_type(self):
         return self.table_cols[0].text
@@ -226,7 +231,8 @@ class EventStatusInfo(EventInfo):
 
         self.col_names = [col.text for col in header.find_all('td')]
 
-        if len(self.col_names) == 0: return None
+        if len(self.col_names) == 0:
+            return None
 
         # rows to list of dicts
         self.unit_table = [{self.col_names[idx]:col.text for idx, col in enumerate(
@@ -246,7 +252,7 @@ class EventDescInfo(EventInfo):
 
 class Event(object):
     def __init__(self, demarcation_tag):
-        
+
         self._event_info_html = event_html_from_demarc_tag(
             demarcation_tag, 0)
         self.eci = EventCategoricalInfo(self._event_info_html)
@@ -265,7 +271,7 @@ class Event(object):
             self._event_desc_html = event_html_from_demarc_tag(
                 demarcation_tag, 1)
             self.edi = EventDescInfo(self._event_desc_html)
-        
+
         self.event_number = self.eci.event_number
 
     def __repr__(self):
@@ -277,79 +283,81 @@ class EventNotificationReport(object):
         self.date = dp.parse(page_html.find(id='mainSubFull').find_all('table')[
             0].find('h1').text, fuzzy=True)
         self.main_table = self.get_main_table(page_html)
-        self.events=self.get_events_from_main(self.main_table)
-        self._page_html=page_html
+        self.events = self.get_events_from_main(self.main_table)
+        self._page_html = page_html
         self.num_events = len(self.events)
 
     def __repr__(self):
         return f'Event Notification Report from {str(self.date.date())}. {self.num_events} events, numbers {", ".join([str(x.event_number) for x in self.events])}.'
 
     def get_main_table(self, page_html):
-        return page_html.find(id = 'mainSubFull').find_all('table')
+        return page_html.find(id='mainSubFull').find_all('table')
 
     @classmethod
     def get_events_from_main(cls, main_table):
         # tables appear to have a name that follows <a name="en{eventnumber}"></a>
-        er_demarcation_tags=main_table[0].find_all(
+        er_demarcation_tags = main_table[0].find_all(
             'a', {"name": re.compile('en')})
-        event_tables_html=[]
+        event_tables_html = []
 
         for tag in er_demarcation_tags:
-            e=Event(tag)
+            e = Event(tag)
             event_tables_html.append(e)
         return event_tables_html
 
     @classmethod
     def from_url(cls, url: str, headers) -> EventNotificationReport:
-        req=None
-        attempts=0
+        req = None
+        attempts = 0
         while req is None and attempts < 5:
-            req=requests.get(url, timeout = 5, headers = headers)
+            req = requests.get(url, timeout=5, headers=headers)
         req.raise_for_status()
         if not req:
             raise ValueError('Unable to fetch url')
-        page_html=BeautifulSoup(req.content, 'html.parser')
+        page_html = BeautifulSoup(req.content, 'html.parser')
         return cls(page_html)
 
 
 # get all dates
 def build_nrc_event_report_url(year, month, day):
-    month, day=str(month).zfill(2), str(day).zfill(2)
-    url=f'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/{year}/{year}{month}{day}en.html'
+    month, day = str(month).zfill(2), str(day).zfill(2)
+    url = f'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/{year}/{year}{month}{day}en.html'
     return url
 
-def generate_nrc_event_report_urls(start_year = 2004, end_year = datetime.date.today().year):
-    dates={}
+
+def generate_nrc_event_report_urls(start_year=2004, end_year=datetime.date.today().year):
+    dates = {}
     for year in range(start_year, end_year):
         # years before 2003 are in a weird format
         for month in range(1, 13):
-            day_range=calendar.monthrange(year, month)
+            day_range = calendar.monthrange(year, month)
             for day in range(day_range[0], day_range[1]):
-                dates[(year, month, day)]=build_nrc_event_report_url(
+                dates[(year, month, day)] = build_nrc_event_report_url(
                     year, month, day)
     return dates
 
+
 if __name__ == "__main__":
 
-    url='https://www.nrc.gov/reading-rm/doc-collections/event-status/event/2005/20050606en.html'
+    url = 'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/2005/20050606en.html'
 
     e = EventNotificationReport.from_url(url, headers)
 
-    url2 ='https://www.nrc.gov/reading-rm/doc-collections/event-status/event/2017/20171129en.html'
+    url2 = 'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/2017/20171129en.html'
 
     f = EventNotificationReport.from_url(url2, headers)
 
     url3 = 'https://www.nrc.gov/reading-rm/doc-collections/event-status/event/2005/20050607en.html'
 
     g = EventNotificationReport.from_url(url3, headers)
-    
+
     er_urls = generate_nrc_event_report_urls()
 
-    from random import sample 
+    from random import sample
 
     urls = sample(list(er_urls.values()), 10)
 
-    #loop the urls and skip any 404s
+    # loop the urls and skip any 404s
 
     def fetch_enrs(urls):
         error_list = []
